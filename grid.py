f145a175b8ad10bd26b018a0b1b7f7f3da2041bc
mrcolc <meric_olcay2004@hotmail.com>
2024-05-31 15:43:15 +0300

    Comments added to grid.py and snake.py

--- grid.py
+++ grid.py
@@ -1,9 +1,11 @@
-import os
+import os  # to get the path of a file
 import pygame
 import random
 
-# loading photo at the start
+# loading photos from the lib at the start
+# getting the current path
 current_dir = os.path.dirname(os.path.realpath(__file__))
+# adding folder name and the file name to current directory to access pngs
 background = pygame.image.load(os.path.join(current_dir, "lib", "background.png"))
 right_panel = pygame.image.load(os.path.join(current_dir, "lib", "right_panel.png"))
 food = pygame.image.load(os.path.join(current_dir, "lib", "food.png"))
@@ -12,67 +14,95 @@ snakes_head = pygame.image.load(os.path.join(current_dir, "lib", "snake_head.png
 snakes_body = pygame.image.load(os.path.join(current_dir, "lib", "snake_body.png"))
 wall_image = pygame.image.load(os.path.join(current_dir, "lib", "wall_image.png"))
 
+
+# The Grid class to represent the snake in the grid and the right panel
 class Grid:
     def __init__(self, frame_size_x, frame_size_y):
+        # assigning x and y size of the frame
         self.frame_size_x = frame_size_x
         self.frame_size_y = frame_size_y
+        # creating the food position
         self.food_pos = [random.randrange(10, (frame_size_x // 10) - 1) * 10,
                          random.randrange(10, (frame_size_y // 10) - 1) * 10]
+        # bomb position to be -1,-1 that means off the grid
         self.bomb_pos = [-1, -1]
+        # wall positions array created
         self.wall_positions = []
+        # random wall creating mechanism
         while len(self.wall_positions) < 10:
+            # creating random x and y values
             x = random.randrange(10, (frame_size_x // 10) - 1) * 10
             y = random.randrange(10, (frame_size_y // 10) - 1) * 10
-            # Check if there is a wall already in this position or within a 10-pixel gap
+            # check if there is a wall already in this position or within a 10-pixel gap
             if all(abs(x - w[0]) > 10 or abs(y - w[1]) > 10 for w in self.wall_positions):
                 self.wall_positions.append((x, y))
 
+    # A method to spawn the food in the grid
     def spawn_food(self, snake_body):
+        # creating random positions for the food
         self.food_pos = [random.randrange(10, (self.frame_size_x // 10) - 1) * 10,
                          random.randrange(10, (self.frame_size_y // 10) - 1) * 10]
-        
+
+        # checking if the position of the food inside the walls
         for pos in self.wall_positions:
             if self.food_pos[0] == pos[0] and self.food_pos[1] == pos[1]:
-                self.spawn_food(snake_body)
-                
-        for pos in snake_body:
-            if self.food_pos[0] == pos[0] and self.food_pos[1] == pos[1]:
+                # if it is create new random values for position of the food
                 self.spawn_food(snake_body)
 
+        # checking if the position of the food inside the snake's body
+        for pos in snake_body:
+            if self.food_pos[0] == pos[0] and self.food_pos[1] == pos[1]:
+                # if it is create new random values for position of the food
+                self.spawn_food(snake_body)
+
+    # A method to spawn the bomb on the grid
     def spawn_bomb(self, snake_body):
+        # creating random position for the bomb
         self.bomb_pos = [random.randrange(10, (self.frame_size_x // 10) - 1) * 10,
                          random.randrange(10, (self.frame_size_y // 10) - 1) * 10]
 
+        # checking if the position of the bomb inside the walls
         for pos in self.wall_positions:
             if self.bomb_pos[0] == pos[0] and self.bomb_pos[1] == pos[1]:
+                # if it is create new random values for position of the bomb
                 self.spawn_bomb(snake_body)
-                
+
+        # checking if the position of the bomb inside the snake's body
         for pos in snake_body:
             if self.bomb_pos[0] == pos[0] and self.bomb_pos[1] == pos[1]:
+                # if it is create new random values for position of the bomb
                 self.spawn_bomb(snake_body)
 
+        # checking if the position of the bomb is the same with the food position
         if self.bomb_pos == self.food_pos:
+            # if it is create new random values for position of the bomb
             self.spawn_bomb(snake_body)
 
+    # A amethod to draw the grid
     def draw(self, game_window, snake_body, snake_direction, current_score, high_scores, ai_high_score):
+        # adjusting the background
         game_window.blit(background, (0, 0))
+        # draw the walls based on the position of the walls
         for pos in self.wall_positions:
             game_window.blit(wall_image, (pos[0], pos[1]))
-            
+
+        # create the right panel
         game_window.blit(right_panel, (720, 0))
+        # draw the food on the grid
         game_window.blit(food, (self.food_pos[0], self.food_pos[1]))
+        # if the bomb positions are not -1,-1, draw the bomb on the grid
         if self.bomb_pos[0] != -1 and self.bomb_pos[1] != -1:
             game_window.blit(bomb, (self.bomb_pos[0], self.bomb_pos[1]))
-        
-        # Draw the score in the right panel
+
+        # write the current score on the right panel
         font = pygame.font.Font('freesansbold.ttf', 22)
-        
         text = font.render("Score: ", True, (255, 255, 255))
         game_window.blit(text, (740, 60))
         score = font.render(str(current_score), True, (255, 255, 255))
         game_window.blit(score, (815, 60))
 
-        text = font.render("Records" , True, (255, 255, 255))
+        # write the top 3 high scores on the right panel
+        text = font.render("Records", True, (255, 255, 255))
         game_window.blit(text, (745, 110))
         high_1 = font.render("1st: " + str(high_scores[0]), True, (255, 255, 255))
         game_window.blit(high_1, (755, 140))
@@ -81,11 +111,13 @@ class Grid:
         high_3 = font.render("3rd: " + str(high_scores[2]), True, (255, 255, 255))
         game_window.blit(high_3, (755, 200))
 
-        text = font.render("AI Record" , True, (255, 255, 255))
+        # write the AI's high score on the right panel
+        text = font.render("AI Record", True, (255, 255, 255))
         game_window.blit(text, (740, 240))
         high_ai = font.render(str(ai_high_score), True, (255, 255, 255))
         game_window.blit(high_ai, (785, 270))
 
+        # write instructions on the right panel
         text = font.render("Press P", True, (255, 255, 255))
         game_window.blit(text, (750, 310))
         text = font.render("to pause", True, (255, 255, 255))
@@ -93,36 +125,50 @@ class Grid:
         text = font.render("or exit", True, (255, 255, 255))
         game_window.blit(text, (750, 370))
 
+        # based on the direction draw the snake body
         flag = 0
         for pos in snake_body:
             if flag == 0:
                 match snake_direction:
+                    # for the snake head
                     case 'UP':
+                        # if it is up draw the snake head
                         game_window.blit(snakes_head, (pos[0], pos[1]))
                     case 'DOWN':
+                        # if it is down, rotate the snake head 180 degrees and draw
                         game_window.blit(pygame.transform.rotate(snakes_head, 180), (pos[0], pos[1]))
                     case 'LEFT':
+                        # if it is left, rotate the snake head 90 degrees and draw
                         game_window.blit(pygame.transform.rotate(snakes_head, 90), (pos[0], pos[1]))
                     case 'RIGHT':
+                        # if it is right, rotate the snake head -90 degrees and draw
                         game_window.blit(pygame.transform.rotate(snakes_head, -90), (pos[0], pos[1]))
-                flag = 1
+                flag = 1 # setting flag to 1, that means snake head is drawn
             else:
+                # draw the snake body
                 game_window.blit(snakes_body, (pos[0], pos[1]))
 
+    # A method to checking the collision of the snake with the walls
     def check_collision(self, snake_pos):
+        # checking if the snake hit the inner random walls
         for pos in self.wall_positions:
             if snake_pos[0] == pos[0] and snake_pos[1] == pos[1]:
                 return True
+        # checking if the snake hit the outer walls
         return (snake_pos[0] < 10 or snake_pos[0] > self.frame_size_x - 20 or
                 snake_pos[1] < 10 or snake_pos[1] > self.frame_size_y - 20)
 
+    # A method to increase the score of the snake
     def increase_score(self, snake):
         snake.snake_score += 1
 
+    # A method to decrease the score of the snake
     def decrease_score(self, snake):
         snake.snake_score -= 2
 
+    # A method to checking the collision of the snake with itself
     def check_self_collision(self, snake_body):
+        # checking if the head is in the body
         for block in snake_body[1:]:
             if snake_body[0][0] == block[0] and snake_body[0][1] == block[1]:
                 return True
--- snake.py
+++ snake.py
@@ -2,55 +2,79 @@ import pygame
 import numpy as np
 from enum import Enum
 
+
+# The Snake Class to represent snake in the game grid
 class Snake:
     def __init__(self):
         # snake_pos[0] is x pos
         # snake_pos[1] is y pos
         self.snake_pos = [100, 50]
+        # position of the snake's body
         self.snake_body = [[100, 50], [100 - 10, 50], [100 - (2 * 10), 50]]
+        # setting score to be 0
         self.snake_score = 0
         # -------- STATE SPACE VARIABLES --------
         # danger zone for snake head. (left, right, below, and above of snake's head)
-        self.dangerzone = [[self.snake_pos[0] - 10, self.snake_pos[1]], # left
-                           [self.snake_pos[0] + 10, self.snake_pos[1]], # right
-                           [self.snake_pos[0], self.snake_pos[1] - 10], # below
-                           [self.snake_pos[0], self.snake_pos[1] + 10]] # above
+        self.dangerzone = [[self.snake_pos[0] - 10, self.snake_pos[1]],  # left
+                           [self.snake_pos[0] + 10, self.snake_pos[1]],  # right
+                           [self.snake_pos[0], self.snake_pos[1] - 10],  # below
+                           [self.snake_pos[0], self.snake_pos[1] + 10]]  # above
+        # direction to be right initially
         self.direction = Direction.RIGHT
+        # shows to next direction
         self.change_to = self.direction
 
+    # A method to change the direction of the snake
     def change_direction(self, event):
+        # if arrow up or w pressed
         if event.key == pygame.K_UP or event.key == ord('w'):
+            # change direction to the up
             self.change_to = Direction.UP
+        # if arrow down or s pressed
         if event.key == pygame.K_DOWN or event.key == ord('s'):
+            # change direction to the down
             self.change_to = Direction.DOWN
+        # if arrow left or a pressed
         if event.key == pygame.K_LEFT or event.key == ord('a'):
+            # change direction to the left
             self.change_to = Direction.LEFT
+        # if arrow right or r pressed
         if event.key == pygame.K_RIGHT or event.key == ord('d'):
+            # change direction to the right
             self.change_to = Direction.RIGHT
 
+    # A method to predict the next direction of the snake
     def predict_direction(self, action):
+        # array of directions in the clockwise direction
         clockwise = [Direction.RIGHT, Direction.DOWN, Direction.LEFT, Direction.UP]
+        # assigning idx to be current position index in the clockwise direction
         idx = clockwise.index(self.direction)
-        
-        if np.array_equal(action, [1, 0, 0]): # STRAIGHT: No change
+
+        if np.array_equal(action, [1, 0, 0]):  # STRAIGHT: No change
             self.change_to = clockwise[idx]
-        elif np.array_equal(action, [0, 1, 0]): # RIGHT: Next direction in the clockwise list 
+        elif np.array_equal(action, [0, 1, 0]):  # RIGHT: Next direction in the clockwise list
             new_idx = (idx + 1) % 4
             self.change_to = clockwise[new_idx]
-        elif np.array_equal(action, [0, 0, 1]): # LEFT: Previous direction in the clockwise list
+        elif np.array_equal(action, [0, 0, 1]):  # LEFT: Previous direction in the clockwise list
             new_idx = (idx - 1) % 4
             self.change_to = clockwise[new_idx]
 
+    # A method to move the snake on the grid
     def move(self):
+        # change direction to Up
         if self.change_to == Direction.UP and self.direction != Direction.DOWN:
             self.direction = Direction.UP
+        # change direction to Down
         elif self.change_to == Direction.DOWN and self.direction != Direction.UP:
             self.direction = Direction.DOWN
+        # change direction to Left
         elif self.change_to == Direction.LEFT and self.direction != Direction.RIGHT:
             self.direction = Direction.LEFT
+        # change direction to Right
         elif self.change_to == Direction.RIGHT and self.direction != Direction.LEFT:
             self.direction = Direction.RIGHT
 
+        # Based on the direction update the snake's position
         if self.direction == Direction.UP:
             self.snake_pos[1] -= 10
         elif self.direction == Direction.DOWN:
@@ -61,29 +85,40 @@ class Snake:
             self.snake_pos[0] += 10
 
         # update danger zone (left, right, below, and above of snake's head)
-        self.dangerzone = [[self.snake_pos[0] - 10, self.snake_pos[1]], # left
-                           [self.snake_pos[0] + 10, self.snake_pos[1]], # right
-                           [self.snake_pos[0], self.snake_pos[1] - 10], # below
-                           [self.snake_pos[0], self.snake_pos[1] + 10]] # above
+        self.dangerzone = [[self.snake_pos[0] - 10, self.snake_pos[1]],  # left
+                           [self.snake_pos[0] + 10, self.snake_pos[1]],  # right
+                           [self.snake_pos[0], self.snake_pos[1] - 10],  # below
+                           [self.snake_pos[0], self.snake_pos[1] + 10]]  # above
 
+    # A method to grow the length of the snake when the food is eaten
     def grow(self, food_pos):
+        # increase the body length by one
         self.snake_body.insert(0, list(self.snake_pos))
+        # if the food and the snake are in the same area, that means food is eaten
         if self.snake_pos[0] == food_pos[0] and self.snake_pos[1] == food_pos[1]:
+            # return true
             return True
         else:
+            # if it is not, decrement the size by one
             self.snake_body.pop()
+            # return false
             return False
 
+    # A method to shrink the length of the snake when the bomb is eaten
     def shrink(self, bomb_pos):
+        # if the bomb and the snake are in the same area, that means bomb is eaten
         if self.snake_pos[0] == bomb_pos[0] and self.snake_pos[1] == bomb_pos[1]:
+            # decrement the size of the snake by two
             self.snake_body.pop()
             self.snake_body.pop()
+            # bomb position to be -1,-1 which is deleted in the grid
             bomb_pos[0], bomb_pos[1] = -1, -1
             return True
         return False
 
+
+# The Direction class to present the direction in enum
 class Direction(Enum):
-    #3643
     RIGHT = 1
     LEFT = 2
     UP = 3
